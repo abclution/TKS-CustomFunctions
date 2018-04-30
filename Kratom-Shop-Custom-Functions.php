21ca53fa576607360fce96a5ca041aaebf0e0aa9
abclution <andreas.tyrosvoutis@gmail.com>
2018-04-30 20:26:51 +0300

    Update Kratom-Shop-Custom-Functions.php

--- Kratom-Shop-Custom-Functions.php
+++ Kratom-Shop-Custom-Functions.php
@@ -10,9 +10,10 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
  * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/abclution/TKS-CustomFunctions.git
  */
 
-
+/* WOOCOMMERCE */
 /* Show more variations on product back end */
 
 add_filter( 'woocommerce_admin_meta_boxes_variations_per_page', 'handsome_bearded_guy_increase_variations_per_page' );
@@ -20,18 +21,27 @@ function handsome_bearded_guy_increase_variations_per_page() {
 	return 500;
 }
 
+/* WOOCOMMERCE */
 /* //Increase in the number of variations to 150  */
 define( 'WC_MAX_LINKED_VARIATIONS', 500 );
-
 /* Read more at: https://www.proy.info/create-more-than-50-product-variations-in-woocommerce/  */
 
-
+/* WOOCOMMERCE */
+/* UNCHECK SHIP TO DIFFERENT ADDRESS ON CHECKOUT */
 add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
+
+/* WOOCOMMERCE */
+/* # Hide out of stock variations # https://stackoverflow.com/questions/46407618/hide-out-of-stock-variations-woocommerce */
+function custom_wc_ajax_variation_threshold( $qty, $product ) {
+return 500; // Enter value that is higher then possible variations.
+}
+add_filter( 'woocommerce_ajax_variation_threshold', 
+'custom_wc_ajax_variation_threshold', 10, 2 );
+
 
 
 /* Expose shop orders to types and views */
 /* https://wp-types.com/forums/topic/woocommerce-shop_order/  */
-
 add_action('init', 'wc_orders_public');
 function wc_orders_public() {
         global $wp_post_types;
@@ -44,43 +54,8 @@ function wc_orders_public() {
 
 
 /* EONET MANUAL USER APPROVE PREVENT PASSWORD RESET UPON APPROVAL */
-
 add_filter( 'eonet_mua_avoid_password_reset', '__return_true');
 
-/* # Hide out of stock variations # https://stackoverflow.com/questions/46407618/hide-out-of-stock-variations-woocommerce */
 
-function custom_wc_ajax_variation_threshold( $qty, $product ) {
-return 500; // Enter value that is higher then possible variations.
-}
-
-add_filter( 'woocommerce_ajax_variation_threshold', 
-'custom_wc_ajax_variation_threshold', 10, 2 );
-
-
-
-
-/*
-add_filter( ‘restricted_site_access_is_restricted’, ‘my_rsa_feed_override’, 10, 2 );
-function my_rsa_feed_override( $is_restricted, $wp ) {
-// check query variables to see if this is the feed
-if ( is_wc_endpoint_url( ‘lost-password’ ) ) {
-$is_restricted = false;
-}
-return $is_restricted;
-}
-*/
-
-/* add_filter( 'restricted_site_access_is_restricted', 'my_restricted_check' );
-function my_restricted_check( $is_restricted ) {
-    global $wp;
-    if (
-        is_wc_endpoint_url( ‘lost-password’ )
-   	$wp->query_vars['pagename'] == 'register' ||
-    	$wp->query_vars['name'] == 'a-name-of-a-post'
-    )
-		$is_restricted = false;
-
-    return $is_restricted;
-*/
 
 
