505687c68241f627bc1543dec272a14bd24c4f07
abclution <andreas.tyrosvoutis@gmail.com>
2018-05-01 15:46:43 +0300

    Added editable in any status to orders

--- Kratom-Shop-Custom-Functions.php
+++ Kratom-Shop-Custom-Functions.php
@@ -13,6 +13,21 @@
  * GitHub Plugin URI: https://github.com/abclution/TKS-CustomFunctions.git
  */
 
+
+
+/* https://nicola.blog/2015/05/14/how-to-edit-processing-orders/   */
+/* WOOCOMMERCE */
+/* ENABLE EDITING FOR "PROCCESSING ORDERS" */
+
+add_filter( 'wc_order_is_editable', 'wc_make_processing_orders_editable', 10, 2 );
+function wc_make_processing_orders_editable( $is_editable, $order ) {
+    if ( $order->get_status() == 'processing' ) {
+        $is_editable = true;
+    }
+
+    return $is_editable;
+}
+
 /* WOOCOMMERCE */
 /* Show more variations on product back end */
 
